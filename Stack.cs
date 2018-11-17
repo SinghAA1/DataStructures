9646b9f810924ad7c316b62cd73e778ef3d2b4d8
SinghAA1 <A.A.Singh1@lse.ac.uk>
2018-11-17 21:48:53 +0000

    Formatting

--- Stack.cs
+++ Stack.cs
@@ -2,42 +2,42 @@
 
 namespace Stacks
 {
-	public class Stack<T>
-	{
-		Node first;
+    public class Stack<T>
+    {
+        Node first;
 
-		public bool IsEmpty()
-		{
-			return first == null;
-		}
+        public bool IsEmpty()
+        {
+            return first == null;
+        }
 
-		public void Push(T newHead)
-		{
-			var temp = new Node { Item = newHead, Next = first };
-			first = temp;
-		}
+        public void Push(T newHead)
+        {
+            var temp = new Node { Item = newHead, Next = first };
+            first = temp;
+        }
 
-		public T Pop()
-		{
-            if(IsEmpty())
+        public T Pop()
+        {
+            if (IsEmpty())
             {
                 throw new StackUnderflowException();
             }
 
-			var headItem = first.Item;
-			first = first.Next;
-			return headItem;
-		}
+            var headItem = first.Item;
+            first = first.Next;
+            return headItem;
+        }
 
-		class Node
-		{
-			public T Item { get; set; }
-			public Node Next { get; set; }
-		}
-	}
+        class Node
+        {
+            public T Item { get; set; }
+            public Node Next { get; set; }
+        }
+    }
 
     public class StackUnderflowException : Exception
     {
-        
+
     }
 }
